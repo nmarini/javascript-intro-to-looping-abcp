e1a037c667b487adeaaab2b7756228f2e3b56b8f
Nicolas Marini <nmarini44@gmail.com>
2018-02-01 15:48:54 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,4 +1,12 @@
-function forLoop(array) { for (var i = 0; i <25; i++) 
-{ if (i === 1) {console.log(`I am 1 strange loop`) }
+function forLoop(array) { for 
+(var i = 0; i <25; i++) 
+{ if 
+(i === 1) 
+{console.log(`I am 1 strange loop`) 
+  }
   else { console.log(`I am ${1} strange loops.`)
-}}}
+}
+  
+}
+  
+}
